e551911d24b0cf06a436dd310022e70e08731a9e
rkaur <rkaur@hcn-inc.com>
2020-03-27 12:11:17 -0400

    New Project

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+   <h1>Hello</h1>
+   {{data}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     data:''
+    }
+  },
+  methods:{
+    getAllCases(){
+      axios.get('https://corona.lmao.ninja/all')
+      .then((res)=>{
+       this.data = res.data
+      })
+      .catch(error=>{
+        console.error('Notworking')
+      })
+    }
+  },
+  created(){
+    this.getAllCases();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
