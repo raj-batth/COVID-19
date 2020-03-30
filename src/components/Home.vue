9354025bb8ca02e3b4af1acb182f723397ea436f
rkaur <rkaur@hcn-inc.com>
2020-03-30 00:35:02 -0400

    Finished Data Displaying in GraphicalForm

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,26 +8,30 @@
       header="Total Cases"
       header-tag="header">
         <b-card-text>
-        {{getData.cases | number}}
+        {{getData.cases}}
         </b-card-text>
       </b-card>
         <b-card
-      style="background-color:#F8D7DA"
+      style="background-color:#F5C6CB"
       header="Total Deaths"
       header-tag="header">
         <b-card-text variant="success">
-        {{getData.deaths | number}}
+        {{getData.deaths}}
         </b-card-text>
       </b-card>
        <b-card
       style="background-color:#D4EDDA"
-      header="Total Recoved"
+      header="Recoved"
       header-tag="header">
         <b-card-text variant="success">
-        {{getData.recovered | number}}
+        {{getData.recovered}}
         </b-card-text>
       </b-card>
       </b-row>
+
+      <div class="small my-5">
+        <line-chart :chart-data="datacollection" :options="chartOptions"></line-chart>
+      </div>
     </b-container>
   </div>
 </template>
@@ -35,11 +39,18 @@
 <script>
 
 import {mapActions} from 'vuex';
+import LineChart from '../charts/charts'
+import axios from 'axios'
 export default {
-  name: 'HelloWorld',
+  components: {
+      LineChart
+    },
   data () {
     return {
-     data:''
+     data:'',
+     datacollection: null,
+     chartOptions: null,
+     loaded:false
     }
   },
   computed: {
@@ -47,6 +58,38 @@ export default {
       return this.$store.getters.data
     }
   },
+  async mounted () {
+    try {
+      axios.get('https://corona.lmao.ninja/v2/historical/all')
+        .then((res)=>{
+          this.datacollection = {
+            labels: Object.keys(res.data.cases),
+            datasets: [
+              {
+                label: 'Total Cases',
+                backgroundColor: '#D1ECF0',
+                data:Object.values(res.data.cases)
+              },
+              {
+                label: 'Total Deaths',
+                backgroundColor: '#F5C6CB',
+                data: Object.values(res.data.deaths)
+              },
+            ],
+            chartOptions: {
+              responsive: true,
+              maintainAspectRatio: true
+            }
+
+          }
+        }).finally(()=>{this.loaded = true})
+        .catch(error=>{
+          console.log(error)
+        })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }
 </script>
 <style scoped>
